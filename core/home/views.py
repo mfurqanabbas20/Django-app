ca7c2ef582e93121103f009d234997a0949378eb
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-13 10:09:26 +0500

    initial commit

---
+++ core/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("""
+       <h1 style="color: red">Hey, I am a Django Server</h1>
+    """)
+
+def success_page(request):
+    # data hm context k zariya phonchaiyein gaye template mei 
+    people = [
+        {"name": "Furqan", "age": 22},
+        {"name": "Furqan", "age": 21},
+        {"name": "Furqan", "age": 20}
+    ]
+    print("*" * 10)
+    return render(request, "index.html", context={'peoples', people})
