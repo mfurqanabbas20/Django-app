30d230b7182ed1552a72c8d0e1f7527fadfb0738
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-14 10:15:29 +0500

    understand migrations

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = model.AutoField()
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length = 8)
+    grade = models.IntegerField(null=True, blank=True)
+    image = models.ImageField()
+    file = models.FileField()
