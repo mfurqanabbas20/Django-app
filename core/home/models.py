dfeb53667c374434be99b9bdc3e87cdc1a3d7afe
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-15 10:21:01 +0500

    practice django cru

--- core/home/models.py
+++ core/home/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+# Car.objects.create()
+
+# Car.objects.all()
+
+# Car.objects.get(id=1)
+
+# Car.objects.filter(id=1) => []
+
+# first get car then update info and save
+
+# Car.objects.filter(id=1).update(car_name = "Creta")
+
+
 class Student(models.Model):
     # id = model.AutoField()
     name = models.CharField(max_length=255)
@@ -12,3 +25,10 @@ class Student(models.Model):
     file = models.FileField()
 
 # models = model maanger
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
