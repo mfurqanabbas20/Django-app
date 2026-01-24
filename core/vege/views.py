ba9ed04829da4fa8b8ec7ec602a4cdf8f2f9ca50
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-01-24 22:40:11 +0500

    encrypt password and create user

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -33,4 +35,27 @@ def login_page(request):
 def register_page(request):
     if request.method == "POST":
         first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("user_name")
+        password = request.POST.get("password")
+    
+    user = User.objects.filter(username = username)
+
+    if user.exists():
+        messages.info(request, 'Username already taken')
+        return redirect("/register/")
+
+
+
+    user = User.objects.create(
+        first_name,
+        last_name,
+        username,
+    )
+    # this is to encrypt the password
+    user.set_password(password)
+
+    user.save()
+
+
     return render(request, "login.html")
